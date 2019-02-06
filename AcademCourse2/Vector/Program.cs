ba43a94b90c4e7571de871cbd235f524596b0bee
romangubkin1995 <romangubkin1995@gmail.com>
2019-02-06 21:39:34 +0700

    Реализовал класс Vector

--- AcademCourse2/Vector/Program.cs
+++ AcademCourse2/Vector/Program.cs
@@ -10,11 +10,25 @@ namespace Vector
     {
         static void Main(string[] args)
         {
-            int[] array = new int [ ] {1,2,3,4,5 };
+            double[] array = new double[] { 1, 2, 3, 4, 5 };
 
-            int [] arrayCopy = new int[] { };
-            array[0] = 5;
-            Console.WriteLine(arrayCopy[0]);
+            Vector vector1 = new Vector(5);
+            Vector vector2 = new Vector(array);
+            Vector vector3 = new Vector(7, array);
+            Vector vector4 = new Vector(vector2);
+
+            Console.WriteLine($"Размерность вектора2 = {vector2.GetSize()}");
+            Console.WriteLine($"Прибавление вектора1 к вектору3 = {vector1.AddVector(vector3).ToString()}");
+            Console.WriteLine($"Вычитание вектора3 из вектора1 = {vector1.ExceptVector(vector3).ToString()}");
+            Console.WriteLine($"Умножение вектора4 на скаляр = {vector4.MultipliedByNumber(5).ToString()}");
+            Console.WriteLine($"Разворот вектора = {vector2.TurnVector().ToString()}");
+            Console.WriteLine($"Модуль вектора3 = {vector3.GetVectorsLength()}");
+            Console.WriteLine($"Компонент вектора4 под индексом - 3 = {vector4.GetElement(3)}");
+            Console.WriteLine($"Сложение вектора1 и вектора3 = {Vector.AdditionTwoVectors(vector1, vector3).ToString()}");
+            Console.WriteLine($"Вычитание из вектора1 вектора3 = {Vector.SubtractionTwoVectors(vector1, vector3).ToString()}");
+            Console.WriteLine($"Скалярное произведение вектора3 и вектора4 = {Vector.ScalarMultiplicationTwoVectors(vector3, vector4).ToString()}");
+            Console.WriteLine($"Сравнивание вектора4 с вектором2 - {vector4.Equals(vector2)}");
+
             Console.ReadKey();
         }
     }
--- AcademCourse2/Vector/Vector.cs
+++ AcademCourse2/Vector/Vector.cs
@@ -8,17 +8,339 @@ namespace Vector
 {
     class Vector
     {
-        public int N { get; set; }
-        public double [] Array;
+        private int ArrayLength;
+        public double[] VectorsArray { get; set; }
 
-        public Vector(int n)
+        public Vector(int newArrayLength)
         {
-            double [] Vector = new double [n];
+            if (newArrayLength <= 0)
+            {
+                throw new ArgumentException("Количество компонент вектора должно быть больше нуля.");
+            }
+            ArrayLength = newArrayLength;
+            VectorsArray = new double[ArrayLength];
+
+            for (int i = 0; i < ArrayLength; i++)
+            {
+                VectorsArray[i] = 0;
+            }
         }
 
-        public Vector( Vector vectorCopy)
+        public Vector(double[] array)
         {
-            
+            VectorsArray = new double[array.Length];
+            ArrayLength = array.Length;
+            for (int i = 0; i < array.Length; i++)
+            {
+                VectorsArray[i] = array[i];
+            }
+        }
+
+        public Vector(int newArrayLength, double[] array)
+        {
+            VectorsArray = new double[newArrayLength];
+            ArrayLength = newArrayLength;
+
+            for (int i = 0; i < newArrayLength; i++)
+            {
+                if (i >= array.Length)
+                {
+                    VectorsArray[i] = 0;
+                }
+                else
+                {
+                    VectorsArray[i] = array[i];
+                }
+            }
+        }
+
+        public Vector(Vector clonVector)
+        {
+            VectorsArray = new double[clonVector.GetSize()];
+            ArrayLength = clonVector.ArrayLength;
+
+            for (int i = 0; i < VectorsArray.Length; i++)
+            {
+                VectorsArray[i] = clonVector.VectorsArray[i];
+            }
+        }
+
+        public int GetSize()
+        {
+            return ArrayLength;
+        }
+
+        public Vector AddVector(Vector vector)
+        {
+            int newVectorsArrayLength = GetSize() > vector.GetSize() ? GetSize() : vector.GetSize();
+            double[] newVectorsArray = new double[newVectorsArrayLength];
+
+            if (GetSize() > vector.GetSize())
+            {
+
+                for (int i = 0; i < GetSize(); i++)
+                {
+                    if (i >= vector.GetSize())
+                    {
+                        newVectorsArray[i] = VectorsArray[i];
+                    }
+                    else
+                    {
+                        newVectorsArray[i] = VectorsArray[i] + vector.VectorsArray[i];
+                    }
+                }
+            }
+            else if (GetSize() < vector.GetSize())
+            {
+                for (int i = 0; i < vector.GetSize(); i++)
+                {
+                    if (i >= GetSize())
+                    {
+                        newVectorsArray[i] = vector.VectorsArray[i];
+                    }
+                    else
+                    {
+                        newVectorsArray[i] = VectorsArray[i] + vector.VectorsArray[i];
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < GetSize(); i++)
+                {
+                    newVectorsArray[i] = VectorsArray[i] + vector.VectorsArray[i];
+                }
+            }
+
+            return new Vector(newVectorsArray);
+        }
+
+        public Vector ExceptVector(Vector vector)
+        {
+            int newVectorsArrayLength = GetSize() > vector.GetSize() ? GetSize() : vector.GetSize();
+            double[] newVectorsArray = new double[newVectorsArrayLength];
+
+            if (GetSize() > vector.GetSize())
+            {
+                for (int i = 0; i < GetSize(); i++)
+                {
+                    if (i >= vector.GetSize())
+                    {
+                        newVectorsArray[i] = VectorsArray[i];
+                    }
+                    else
+                    {
+                        newVectorsArray[i] = VectorsArray[i] - vector.VectorsArray[i];
+                    }
+                }
+            }
+            else if (GetSize() < vector.GetSize())
+            {
+                for (int i = 0; i < vector.GetSize(); i++)
+                {
+                    if (i >= GetSize())
+                    {
+                        newVectorsArray[i] = -vector.VectorsArray[i];
+                    }
+                    else
+                    {
+                        newVectorsArray[i] = VectorsArray[i] - vector.VectorsArray[i];
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < GetSize(); i++)
+                {
+                    newVectorsArray[i] = VectorsArray[i] - vector.VectorsArray[i];
+                }
+            }
+
+            return new Vector(newVectorsArray);
+        }
+
+        public Vector MultipliedByNumber(double number)
+        {
+            double[] array = new double[GetSize()];
+
+            for (int i = 0; i < GetSize(); i++)
+            {
+                array[i] = VectorsArray[i] * number;
+            }
+
+            return new Vector(array);
+        }
+
+        public Vector TurnVector()
+        {
+            double[] array = new double[GetSize()];
+            for (int i = 0; i < GetSize(); i++)
+            {
+                array[i] = VectorsArray[i] * -1;
+            }
+
+            return new Vector(array);
+        }
+
+        public double GetVectorsLength()
+        {
+            double sum = 0;
+            for (int i = 0; i < GetSize(); i++)
+            {
+                sum += Math.Pow(VectorsArray[i], 2);
+            }
+            return Math.Sqrt(sum);
+        }
+
+        public double GetElement(int index)
+        {
+            return VectorsArray[index];
+        }
+
+        public static Vector AdditionTwoVectors(Vector vector1, Vector vector2)
+        {
+            int newVectorArrayLength = vector1.GetSize() > vector2.GetSize() ? vector1.GetSize() : vector2.GetSize();
+            double[] newVectorsArray = new double[newVectorArrayLength];
+
+            if (vector1.GetSize() > vector2.GetSize())
+            {
+                for (int i = 0; i < vector1.GetSize(); i++)
+                {
+                    if (i >= vector2.GetSize())
+                    {
+                        newVectorsArray[i] = vector1.VectorsArray[i];
+                    }
+                    else
+                    {
+                        newVectorsArray[i] = vector1.VectorsArray[i] + vector2.VectorsArray[i];
+                    }
+                }
+            }
+            else if (vector1.GetSize() < vector2.GetSize())
+            {
+                for (int i = 0; i < vector2.GetSize(); i++)
+                {
+                    if (i >= vector1.GetSize())
+                    {
+                        newVectorsArray[i] = vector2.VectorsArray[i];
+                    }
+                    else
+                    {
+                        newVectorsArray[i] = vector1.VectorsArray[i] + vector2.VectorsArray[i];
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < vector1.GetSize(); i++)
+                {
+                    newVectorsArray[i] = vector1.VectorsArray[i] + vector2.VectorsArray[i];
+                }
+            }
+
+            return new Vector(newVectorsArray);
+        }
+
+        public static Vector SubtractionTwoVectors(Vector vector1, Vector vector2)
+        {
+            int newVectorsArrayLength = vector1.GetSize() > vector2.GetSize() ? vector1.GetSize() : vector2.GetSize();
+            double[] newVectorsArray = new double[newVectorsArrayLength];
+
+            if (vector1.GetSize() > vector2.GetSize())
+            {
+                for (int i = 0; i < vector1.GetSize(); i++)
+                {
+                    if (i >= vector2.GetSize())
+                    {
+                        newVectorsArray[i] = vector1.VectorsArray[i];
+                    }
+                    else
+                    {
+                        newVectorsArray[i] = vector1.VectorsArray[i] - vector2.VectorsArray[i];
+                    }
+                }
+            }
+            else if (vector1.GetSize() < vector2.GetSize())
+            {
+                for (int i = 0; i < vector2.GetSize(); i++)
+                {
+                    if (i >= vector1.GetSize())
+                    {
+                        newVectorsArray[i] = -vector2.VectorsArray[i];
+                    }
+                    else
+                    {
+                        newVectorsArray[i] = vector1.VectorsArray[i] - vector2.VectorsArray[i];
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < vector1.GetSize(); i++)
+                {
+                    newVectorsArray[i] = vector1.VectorsArray[i] - vector2.VectorsArray[i];
+                }
+            }
+
+            return new Vector(newVectorsArray);
+        }
+
+        public static double ScalarMultiplicationTwoVectors(Vector vector1, Vector vector2)
+        {
+            int minArraysLength = vector1.GetSize() < vector2.GetSize() ? vector1.GetSize() : vector2.GetSize();
+            double scalar = 0;
+
+            for (int i = 0; i < minArraysLength; i++)
+            {
+                scalar += vector1.VectorsArray[i] * vector1.VectorsArray[i];
+            }
+
+            return scalar;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(obj, this))
+            {
+                return true;
+            }
+            if (ReferenceEquals(obj, null) || obj.GetType() != GetType())
+            {
+                return false;
+            }
+
+            Vector vector = (Vector)obj;
+            if (GetSize() != vector.GetSize())
+            {
+                return false;
+            }
+
+            bool isEqually = true;
+            for (int i = 0; i < GetSize(); i++)
+            {
+                if (VectorsArray[i] != vector.VectorsArray[i])
+                {
+                    isEqually = false;
+                    break;
+                }
+            }
+            return isEqually;
+        }
+
+        public override int GetHashCode()
+        {
+            int prime = 37;
+            int hash = 1;
+            for (int i = 0; i < VectorsArray.Length; i++)
+            {
+                hash += prime * hash + VectorsArray[i].GetHashCode();
+            }
+            return hash;
+        }
+
+        public override string ToString()
+        {
+            return String.Join(", ", VectorsArray);
         }
     }
 }
