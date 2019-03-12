088f0b43e5621d30b5166746aa933b23052cb90e
romangubkin1995 <romangubkin1995@gmail.com>
2019-03-12 12:52:51 +0700

    Создал класс Односвязного списка

--- AcademCourse2/List/Program.cs
+++ AcademCourse2/List/Program.cs
@@ -10,6 +10,17 @@ namespace List
     {
         static void Main(string[] args)
         {
+
+            SinglyList<int> number = new SinglyList<int>();
+            number.Add(5);
+            number.Add(6);
+            number.Add(5);
+            number.Add(6);
+
+            Console.WriteLine(number.DeleteNote(3));
+            Console.WriteLine(number);
+            Console.WriteLine(number.GetCount());
+            Console.ReadLine();
         }
     }
 }
--- AcademCourse2/List/SinglyList.cs
+++ AcademCourse2/List/SinglyList.cs
@@ -8,21 +8,135 @@ namespace List
 {
     class SinglyList<T>
     {
-        public Note<T> Head { get; set; }
-        private int count = 0;
+        private Note<T> Head { get; set; }
+        private int notesCount = 0;
+
+        public SinglyList()
+        {
+            
+        }
 
         public SinglyList(Note<T> head)
         {
             Head = head;
+            for (Note<T> p = Head; p != null; p = p.Next)
+            {
+                notesCount++;
+            }
         }
 
         public int GetCount()
         {
+            return notesCount;
+        }
+
+        public T GetFirstElement()
+        {
+            return Head.Data;
+        }
+
+        public T GetElement(int index)
+        {
+            T result = Head.Data;
+            int i = 0;
             for (Note<T> p = Head; p != null; p = p.Next)
             {
-                count++;
+                if (i == index)
+                {
+                    result = p.Data;
+                }
+                i++;
             }
-            return count;
-        }    
+            return result;
+        }
+
+        public T SetElement(T element, int index)
+        {
+            T temp = Head.Data;
+            int i = 0;
+            for (Note<T> p = Head; p != null; p = p.Next)
+            {
+                if (i == index)
+                {
+                    temp = p.Data;
+                    p.Data = element;
+                }
+                i++;
+            }
+            return temp;
+        }
+
+        public T DeleteNote(int index)
+        {
+            T temp = Head.Data;
+            int i = 0;
+            if (index == 0)
+            {
+                Head = Head.Next;
+            }
+            else
+            {
+                for (Note<T> p = Head; p != null; p = p.Next)
+                {
+                    if (i == index - 1)
+                    {
+                        if (p.Next.Next == null)
+                        {
+                            temp = p.Next.Data;
+                            p.Next = null;
+                        }
+                        else
+                        {
+                            temp = p.Next.Data;
+                            p.Next = p.Next.Next;
+                        }
+                    }
+                    i++;
+                }
+            }
+            notesCount--;
+            return temp;
+        }
+
+        public void Add(T element)
+        {
+            for (Note<T> p = Head; ; p = p.Next)
+            {
+                if (Head == null)
+                {
+                    Note<T> newHead = new Note<T>(element);
+                    Head = newHead;
+                    break;
+                }
+                else if (p.Next == null)
+                {
+                    p.Next = new Note<T>(element);
+                    break;
+                }
+            }
+            notesCount++;
+        }
+
+        public void AddToTheTop(T element)
+        {
+            Note<T> newNote = new Note<T>(element);
+            newNote.Next = Head;
+            Head = newNote;
+            notesCount++;
+        }
+
+        public void AddToIndex(T element, int index)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            for (Note<T> p = Head; p != null; p = p.Next)
+            {
+                Console.Write(p.Data + "  ");
+            }
+            return null;
+        }
     }
 }
