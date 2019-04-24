7ce221f489c761c09f5b286752f9f1f240a8e1da
romangubkin1995 <romangubkin1995@gmail.com>
2019-04-24 14:36:19 +0700

    Доделал HashTable

---
+++ AcademCourse2/HashTable/HashTable.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    class HashTable<T> : ICollection<T>
+    {
+        List<T>[] items;
+        public int Count { get; private set; }
+        private int modCount = 0;
+
+        public HashTable(int capacity)
+        {
+            items = new List<T>[capacity];
+        }
+
+        public HashTable()
+        {
+            items = new List<T>[8];
+        }
+
+        public void Add(T value)
+        {
+            int index = Math.Abs(value.GetHashCode() % items.Length);
+
+            if (items[index] == null)
+            {
+                items[index] = new List<T>();
+                items[index].Add(value);
+            }
+            else
+            {
+                items[index].Add(value);
+            }
+
+            Count++;
+            modCount++;
+        }
+
+        public void Clear()
+        {
+            items = new List<T>[8];
+            Count = 0;
+        }
+
+        public bool Contains(T value)
+        {
+            int index = Math.Abs(value.GetHashCode() % items.Length);
+
+            return items[index].IndexOf(value) != -1;
+        }
+
+        public bool Remove(T value)
+        {
+            int index = Math.Abs(value.GetHashCode() % items.Length);
+            modCount++;
+            return items[index].Remove(value);
+        }
+
+        public void CopyTo(T [] array, int index)
+        {
+            if (index < 0 || index >= array.Length)
+            {
+                throw new IndexOutOfRangeException("Индекс находится вне границ массива");
+            }
+
+            if(Count > array.Length - index)
+            {
+                throw new ArgumentException("Недостаточно места в массиве");
+            }
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                if(items[i] == null)
+                {
+                    continue;
+                }
+
+                items[i].CopyTo(array,index);
+                index += items[i].Count;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            int temp = modCount;
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (items[i] == null)
+                {
+                    continue;
+                }
+
+                for (int j = 0; j < items[i].Count; j++)
+                {
+
+                    if (modCount != temp)
+                    {
+                        throw new InvalidOperationException("Список менялся за время обхода");
+                    }
+
+                    yield return items[i].ElementAt(j);
+                }
+            }
+        }
+
+        public bool IsReadOnly => false;
+    }
+}
---
+++ AcademCourse2/HashTable/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashTable<int> numbers = new HashTable<int>(4);
+
+            numbers.Add(10);
+            numbers.Add(9);
+            numbers.Add(4);
+            numbers.Add(0);
+            numbers.Add(36);
+
+            IEnumerator<int> enumerator = numbers.GetEnumerator();
+            while (enumerator.MoveNext() != false)
+            {
+                Console.WriteLine(enumerator.Current);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
